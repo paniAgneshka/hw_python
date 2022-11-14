13476dd12424fe77bdbd9dda8fdc5164acdacd79
paniAgneshka <mikhalchukagneshka@gmail.com>
2022-11-14 18:19:45 +0300

    add program str_len.py

--- str_len.py
+++ str_len.py
@@ -7,10 +7,8 @@ parser.add_argument('file_name', type=str, help='Input file name')
 args =parser.parse_args()
 t = open(args.file_name, 'r')
 value=t.read()
-for line in value:
+for line in t.readlines():
     if len(line) > 90:
-        print(len(line))
-        with open('new_text.txt', 'w') as file:
-            wrapper=tw.wrap(text=value, width= 70)
-            print(wrapper)
-        
+        f=open('new_text.txt', 'w')
+        f.write(tw.wrap(text=value, width= 70))
+        break
