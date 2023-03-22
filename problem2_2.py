b33e45d110983fab297e669c900a551dd2b480ce
paniAgneshka <113934664+paniAgneshka@users.noreply.github.com>
2023-03-22 14:44:26 +0300

    Update problem2_2.py

--- problem2_2.py
+++ problem2_2.py
@@ -21,8 +21,7 @@ for p in range(4, 104, 4):
     qualities.append(np.mean(qual))
     sigmas.append(np.std(qual))
 
-plt.xlabel("Proportion of saved coefficients, %")
-plt.ylabel("Quality")
+plt.xlabel("Proportion of  coefficients, %")
 plt.errorbar(range(4, 104, 4), qualities, yerr=sigmas, fmt="o")
 fig = plt.gcf()
 fig.set_size_inches(12, 6)
